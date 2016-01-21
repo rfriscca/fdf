4b7943257105f909bf6519e28df6bea87f9066da
Remi FRISCCA <rfriscca@e3r10p9.42.fr>
2016-01-21 16:15:01 +0100

    added main : create window and escape to exit

--- srcs/main.c
+++ srcs/main.c
@@ -1,24 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.h                                              :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rfriscca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/01/20 16:47:21 by rfriscca          #+#    #+#             */
-/*   Updated: 2016/01/21 15:09:27 by rfriscca         ###   ########.fr       */
+/*   Created: 2016/01/21 13:11:26 by rfriscca          #+#    #+#             */
+/*   Updated: 2016/01/21 15:53:04 by rfriscca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_H
-# define FDF_H
+#include "fdf.h"
+#include <stdio.h>
 
-# define TILE_WIDTH 32
-# define TILE_HEIGHT 32
-# define INIT_POS_X 720
-# define INIT_POS_Y 60
+int		ft_exit(int n)
+{
+	if (n == 53)
+		exit(n);
+	return (0);
+}
 
-# include <stdlib.h>
-# include <mlx.h>
+int		main(int argc, char **argv)
+{
+	void	*mlx;
+	void	*win;
+	int		x;
+	int		y;
+	int		i;
+	int		j;
 
-#endif
+	x = 0;
+	y = 0;
+	i = 0;
+	j = 0;
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 1920, 1080, "FDF");
+	mlx_key_hook(win, ft_exit, 0);
+	mlx_loop(mlx);
+	return (0);
+}
