4b7e5586884a0437a6ced637c1cbd9f9f3f715ef
Remi FRISCCA <rfriscca@e3r9p6.42.fr>
2016-03-02 13:39:46 +0100

    the program stops if the file is longer than 10000 lines

--- srcs/stock_file.c
+++ srcs/stock_file.c
@@ -6,7 +6,7 @@
 /*   By: rfriscca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/29 16:12:08 by rfriscca          #+#    #+#             */
-/*   Updated: 2016/03/02 12:42:38 by rfriscca         ###   ########.fr       */
+/*   Updated: 2016/03/02 13:38:37 by rfriscca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,25 @@ t_line		*stock_file(int fd)
 	t_line		*list_start;
 	t_line		*list;
 	char		*line;
+	int			i;
 
+	i = 0;
 	if ((ret = get_next_line(fd, &line)) == -1)
 		return (NULL);
-	if (ret > 0)
-	{
-		list_start = ft_new_line(line, ft_strlen(line));
-		list = list_start;
-	}
+	if (ret <= 0 ||
+			(list_start = ft_new_line(line, ft_strlen(line))) == (t_line*)0)
+		return (NULL);
+	list = list_start;
 	if ((ret = get_next_line(fd, &line)) == -1)
 		return (NULL);
 	while (ret > 0)
 	{
-		list->next = ft_new_line(line, ft_strlen(line));
-		list = list->next;
-		if ((ret = get_next_line(fd, &line)) == -1)
+		if ((list->next = ft_new_line(line, ft_strlen(line))) == (t_line*)0)
 			return (NULL);
+		list = list->next;
+		if ((ret = get_next_line(fd, &line)) == -1 || i == 10000)
+			return (NULL);
+		++i;
 	}
 	return (list_start);
 }
